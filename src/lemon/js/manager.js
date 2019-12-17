6fa9ad08f2a3c1edf457784a37df6f67c42c1207
lemon <1648670911@qq.com>
2019-12-17 16:32:55 +0800

    后台组件备份

--- src/lemon/js/manager.js
+++ src/lemon/js/manager.js
@@ -18,17 +18,25 @@ class Navigation extends React.Component {
   };
 
   render() {
-      let params = this.props.match.params
-      let openKeys = params.primary
-      let selectedKeys = '#/' + params.primary + '/' + params.secondary
+      // let params = this.props.match.params
+      // let primary = params.primary
+      // let secondary = params.secondary   
+      // let selectedKeys = 'home'
+      // let openKeys = ''   
+      // if(primary != undefined){
+      //   openKeys = primary
+      // }
+      // if(secondary != undefined && primary != undefined){
+      //   selectedKeys = '#/' + params.primary + '/' + params.secondary
+      // }
     return (
       <div style={{ width: 256 }}>
         <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
           <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
         </Button>
         <Menu
-          defaultSelectedKeys={[selectedKeys]}
-          defaultOpenKeys={[openKeys]}
+          // defaultSelectedKeys={[selectedKeys]}
+          // defaultOpenKeys={[openKeys]}
           mode="inline"
           theme="dark"
           inlineCollapsed={this.state.collapsed}
@@ -94,7 +102,7 @@ class ContentRouter extends React.Component{
                 <Switch>
                     <Route exact path="/" component={HouseBriefInf} />
                     <Route exact path="/home" component={App} />
-                    <Route exact path="/landlord" component={App} />
+                    <Route exact path="/landlord" component={LandlordContent} />
                     <Route exact path="/tenant" component={App} />
                     <Route exact path="/houseInf" component={App} />
                 </Switch>
@@ -105,16 +113,17 @@ class ContentRouter extends React.Component{
 
 class NavigationRouter extends React.Component{
     constructor(props){
-        super(props)
+      super(props)
     }
-
     render(){
 
 
         return(
             <HashRouter>
                 <Switch>
-                    <Route exact path="/:primary/:secondary" component={Navigation} />
+                    <Route exact path="/" component={Navigation} />
+                    {/* <Route exact path="/:primary/" component={Navigation} />
+                    <Route exact path="/:primary/:secondary" component={Navigation} /> */}
                 </Switch>
             </HashRouter>
         )
@@ -136,6 +145,79 @@ class App extends React.Component{
     }
 }
 
+
+class HeadItem extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    const label = { xs: 8, sm: 16, md: 24}
+    const item = this.props.item
+    let content = new Array()
+    for(let index in item){
+      let a = <Col span={3} key={index}>{item[index]}</Col>
+      content.push(a)
+    }
+    return(
+      <Row gutter={label}>
+        <Col span={2}></Col>
+        {
+         content
+        }
+        <Col span={2}></Col>
+      </Row>
+    )
+  }
+}
+
+class ContentItem extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  plusHandle = ()=>{alert(123)}
+  closeHandle = ()=>{alert(123)}
+  infoHandle = ()=>{alert(123)}
+  editHandle = ()=>{alert(123)}
+  render(){
+    const plus = <Button type="primary" size="small" icon="plus"></Button>
+    const close = <Button type="danger" size="small" icon="close"></Button>
+    const info = <Button type="default" size="small" icon="info"></Button>
+    const edit = <Button type="dashed" size="small" icon="edit"></Button>
+    const label = { xs: 8, sm: 16, md: 24}
+    return(
+      <Row gutter={label}>
+        <Col span={2}></Col>
+        <Col span={3}>昵称</Col>
+        <Col span={3}>账号</Col>
+        <Col span={3}>邮箱</Col>
+        <Col span={3}>简介</Col>
+        <Col span={2}>{plus}</Col>
+        <Col span={2}>{close}</Col>
+        <Col span={2}>{edit}</Col>
+        <Col span={2}>{info}</Col>
+        <Col span={2}></Col>
+      </Row>
+    )
+  }
+}
+
+class LandlordContent extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    const item = ['昵称', '账号', '邮箱', '简介']
+    const headItemParams = {
+      item: item
+    }
+    return(
+      <div>
+        <HeadItem {...headItemParams}/>
+        <ContentItem/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
